e7953cd30b67d5a74fb4c894f94b7f1010daadfa
MisterCore <Heiko-YouTube@web.de>
2024-04-09 18:13:09 +0200

    Update to Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ plugins {
 group = "org.example"
 version = "1.0.0"
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
 }
@@ -85,7 +83,7 @@ fun configureRun(provider: net.labymod.gradle.core.minecraft.provider.VersionPro
         args("--addon-dev-environment", "true")
     }
 
-    provider.javaVersion = JavaVersion.VERSION_17
+    provider.javaVersion = JavaVersion.VERSION_21
 
     provider.mixin {
         val mixinMinVersion = when (gameVersion) {
